5ce5d9dc47b3649c09e3171dd172bb71be513436
Brian Kuan <bkuan72@gmail.com>
2020-12-26 23:34:43 +1300

    change html use angular material components

--- src/app/components/component.module.ts
+++ src/app/components/component.module.ts
@@ -1,9 +1,19 @@
+import { MatRippleModule } from '@angular/material/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 import { AlertComponent } from './alert/alert.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatInputModule } from '@angular/material/input';
 
 @NgModule({
-    imports: [ CommonModule ],
+    imports: [
+        CommonModule,
+        MatInputModule,
+        MatIconModule,
+        MatButtonModule,
+        MatRippleModule
+    ],
     declarations: [
         AlertComponent
     ],
