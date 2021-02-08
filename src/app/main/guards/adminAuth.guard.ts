0d54a2cb04095a522ec3153460612fb2fcfdcb26
Brian Kuan <bkuan72@gmail.com>
2021-02-08 13:21:12 +1300

    add new admin auth guard

---
+++ src/app/main/guards/adminAuth.guard.ts
@@ -0,0 +1,27 @@
+import { AuthTokenSessionService } from './../../services/auth-token-session/auth-token-session.service';
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router, CanActivate } from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminAuthGuard implements CanActivate {
+  constructor(
+    private router: Router,
+    private _auth: AuthTokenSessionService
+  ) { }
+
+
+
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    console.log ('validating Auth Guard')
+    if (!this._auth.isLoggedIn() || !this._auth.adminUser) {
+      this.router.navigate(['search-modern']);
+      return false;
+    }
+    return true;
+  }
+}
